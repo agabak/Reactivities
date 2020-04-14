985d7eb293eeb8bf44f8d89d7dde34614158148a
Agaba Kabunga <agaba_k@hotmail.com>
2020-04-14 13:28:45 -0500

    add NavLink for highlight the link

--- client-app/src/features/nav/navBar.tsx
+++ client-app/src/features/nav/navBar.tsx
@@ -1,20 +1,20 @@
 import React  from "react";
 import { Menu, Container, Button } from "semantic-ui-react";
 import { observer} from 'mobx-react-lite';
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
 
   return (
     <Menu fixed="top" inverted>
       <Container>
-        <Menu.Item header as={Link} to='/'>
+        <Menu.Item header as={NavLink} exact to='/'>
           <img src="/assets/logo.png"   alt="logo" style={{marginRight:10 }}/>
           Reactivities
         </Menu.Item>
-        <Menu.Item name="Activities" as={Link} to='/activities'/>
+        <Menu.Item name="Activities" as={NavLink} to='/activities'/>
         <Menu.Item>
-         <Button  as={Link} to='/createActivity'
+         <Button  as={NavLink} to='/createActivity'
          positive  content="Create Activity" />
         </Menu.Item>
       </Container>
